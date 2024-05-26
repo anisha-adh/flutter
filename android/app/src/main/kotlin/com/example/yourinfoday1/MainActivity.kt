92793384da92a10e9eaf9302cd6bf73a941a67b1
anisha adhikari <073bex450.anisha@pcampus.edu.np>
2024-05-26 17:45:13 +0545

    Information

---
+++ android/app/src/main/kotlin/com/example/yourinfoday1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yourinfoday1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget { 
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Infomartion Application',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Information'),
+      ),
+      body: Center(child: MyInfo(),
+      ),
+    );
+  }
+}
+
+class MyInfo extends StatelessWidget{
+  const MyInfo({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return const Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Name: Anisha Adhikari',
+          style: TextStyle(fontSize: 24),
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Email: anisha@waterflowtechnology.net',
+          style: TextStyle(fontSize: 24), 
+        ),
+        SizedBox(height: 10),
+        Text(
+          'Occupation: Software Engineer Intern',
+          style: TextStyle(fontSize: 24),
+        ),
+      ],
+    );
+  }
+}
+
